38769e0de6ee7a80350bd4fc712120b55332eb65
Vikrant1020 <98014253+Vikrant1020@users.noreply.github.com>
2025-01-01 23:16:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Check it out!!!!!!!!!--------------- I AM A VERY BAD MANasdasdb gh asd
+          Edit <code>src/App.js</code> and save to reload. Check it out!!!
           
         </p>
         <a
